6a4c15496642fb7f525df2a6cec442dc16336eca
Rahul Yadav <you@example.com>
2025-11-03 08:55:14 +0530

    implementing the contact us page and login signup

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
 
-export default About
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Consider now provided laughter boy landlord dashwood. Often voice
+            and the spoke. No shewing fertile village equally prepare up females
+            as an. That do an case an what plan hour of paid. Invitation is
+            unpleasant astonished preference attachment friendship on.
+          </p>
+
+          <p>
+            Consider now provided laughter boy landlord dashwood. Often voice
+            and the spoke. No shewing fertile village equally prepare up females
+            as an. That do an case an what plan hour of paid. Invitation is
+            unpleasant astonished preference attachment friendship on.
+          </p>
+
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Consider now provided laughter boy landlord dashwood. Often voice
+            and the spoke. No shewing fertile village equally prepare up females
+            as an. That do an case an what plan hour of paid. Invitation is
+            unpleasant astonished preference attachment friendship on.
+          </p>
+        </div>
+      </div>
+
+      <div className="text-4xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,51 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Cart = () => {
   const { products, currency, cartItem } = useContext(ShopContext);
-  return <div>Cart</div>;
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItem) {
+      for (const item in cartItem[items]) {
+        if (cartItem[items][item]) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItem[items][item],
+          });
+        }
+      }
+    }
+
+    setCartData(tempData);
+  }, [cartItem]);
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"} />
+      </div>
+
+      <div className="">
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+
+          return (
+            <div>
+              <div>
+                <img src={products.image} alt="" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
