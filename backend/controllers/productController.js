8ee002bcd3bac7e86aa23acf40bab439016ddcf5
Rahul Yadav <you@example.com>
2025-11-04 14:50:06 +0530

    implememnting the signin and register functionality

---
+++ backend/controllers/productController.js
@@ -0,0 +1,21 @@
+// function for add product
+const addProduct = async () => {
+
+}
+
+//function for List Products
+const listProducts = async () => {
+    
+}
+
+//function for removing product
+const removeProducts = async () => {
+    
+}
+
+//function for single Products
+const singleProduct = async () => {
+    
+}
+
+export default {addProduct,listProducts,removeProducts,singleProduct}
